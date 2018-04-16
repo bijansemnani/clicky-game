7264b8caffcbc267cc4c124806733b3e40f06cd7
Bijan <bijansemnani@Bijans-MacBook-Pro.local>
2018-04-15 18:22:20 -0700

    got the array of images to print and changing the state when user clicks on image

--- src/App.js
+++ src/App.js
@@ -3,33 +3,51 @@ import logo from './logo.svg';
 import friends from "./friends.json";
 import './App.css';
 import Pictures from "./components/Pictures";
+import Wrapper from "./components/Wrapper";
 
+var score = 0;
 class App extends Component {
 
   state = {
     friends: friends
   };
 
-  toggleClick = chosen => {
+  toggleClick = (chosen, id) => {
+    var newObj = friends[id];
+    if(chosen === false){
+      score++;
+      newObj.chosen = true;
+      friends[id] = newObj;
+      this.setState({
+        friends: friends
+      });
+    }
+    else {
+      score = 0;
+      for (var i = 0; i < friends.length; i++) {
+        friends[i].chosen = false;
+      }
+      this.setState({
+        friends: friends
+      });
+    }
 
   };
   render() {
+    console.log(friends[0]);
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        {this.state.friends.map(friend => (
+      <Wrapper>
+        {this.state.friends.map((friend, index )=> (
           <Pictures
+            name={friend.name}
             key={friend.id}
-            id={friend.id}
+            id={index}
             image={friend.image}
             toggleClick={this.toggleClick}
+            chosen={friend.chosen}
           />
         ))}
-
-      </div>
+      </Wrapper>
     );
   }
 }
--- src/components/Pictures/Pictures.js
+++ src/components/Pictures/Pictures.js
@@ -1,7 +1,13 @@
 import React from "react";
+import "./Pictures.css";
 
 const Pictures = props => (
-    <span> Hello beeguy</span>
+  <div className="card" onClick={() => props.toggleClick(props.chosen,props.id)}>
+    <div data-id={props.id} className="img-container">
+      <img alt={props.name} src={props.image} />
+    </div>
+
+  </div>
 );
 
 export default Pictures;
